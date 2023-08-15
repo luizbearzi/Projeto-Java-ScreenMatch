5f3e53ad115519433ad400a505d5506c8e1c8810
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-08-15 00:17:24 -0300

    ScreenMatch finalizado
    finalizado

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -45,6 +47,17 @@ public class Principal {
         calculadora.inclui(novaSerie1);
 
         System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(novoFilme1);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(novaSerie1);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+
 
     }
 }
